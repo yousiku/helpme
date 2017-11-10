48422ebd42ab5c85018a740cc4e5d9edf0939bcb
yousiku <yousiku@qq.com>
2017-11-10 21:53:02 +0800

    添加部分注释

--- main.py
+++ main.py
@@ -1,79 +1,61 @@
 from flask import Flask, request
 import hashlib, traceback
-import xml.etree.ElementTree as ET
-import time
+import receive
+import reply
 
 
 app = Flask(__name__)
-
-
-def verify_source(request):
-    """
-    验证请求对来源是否是微信
-    :param request: flask.request
-    :return: 验证正确返回echostr，否则返回空
-    """
-    data = request.args
-    if "signature" in data and "timestamp" in data and "nonce" in data and "echostr" in data:
-        signature = data["signature"]
-        timestamp = data["timestamp"]
-        nonce = data["nonce"]
-        echostr = data["echostr"]
-        token = "yousiku"
-        l = [token, timestamp, nonce]
-        l.sort()
-        sha1 = hashlib.sha1()
-        for i in l:
-            sha1.update(i.encode())
-        hashcode = sha1.hexdigest()
-        print("handle/GET func: hashcode, signature:", hashcode, signature)
-        if hashcode == signature:
-            return echostr
-        else:
-            return ""
-    else:
-        return ""
 
 
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
     try:
         if request.method == "GET":
-            return verify_source(request)
-
-        elif request.method == "POST":
-            xml_data = request.data
-            print(xml_data)
-            data = ET.fromstring(xml_data.decode())
-            server_user_name = data.find("ToUserName")
-            msg_id = data.find("MsgId")
-            msg_type = data.find("MsgType")
-            user_name = data.find("FromUserName")
-            content = data.find("Content")
-            print("receive>> user: {}, msg type: {}".format(user_name.text, msg_type.text))
-            if msg_type.text == "text":
-                resp = ET.parse("./reply_common.xml")
-            elif msg_type.text == "event":
-                event = data.find("Event")
-                if event.text == "subscribe":
-                    resp = ET.parse("./reply_subscribe.xml")
+            data = request.args
+            print(data)
+            if "signature" in data and "timestamp" in data and "nonce" in data and "echostr" in data:
+                signature = data["signature"]
+                timestamp = data["timestamp"]
+                nonce = data["nonce"]
+                echostr = data["echostr"]
+                token = "yousiku"
+                l = [token, timestamp, nonce]
+                l.sort()
+                sha1 = hashlib.sha1()
+                for i in l:
+                    sha1.update(i.encode())
+                hashcode = sha1.hexdigest()
+                print("handle/GET func: hashcode, signature:", hashcode, signature)
+                if hashcode == signature:
+                    return echostr
                 else:
                     return ""
             else:
                 return ""
-            root = resp.getroot()
-            root.find("ToUserName").text = user_name.text
-            root.find("FromUserName").text = server_user_name.text
-            root.find("CreateTime").text = str(int(time.time()))
-            r_str = ET.tostring(root, encoding="utf-8")
-            print(r_str)
-            return r_str
+        elif request.method == "POST":
+            xml_data = request.data
+            rec_msg = receive.parse_xml(xml_data)
+            if isinstance(rec_msg, receive.Msg):
+                user = rec_msg.from_user_name
+                me = rec_msg.to_user_name
+                if rec_msg.msg_type == "text":
+                    content = "等那个脑残程序员把代码写完我再来找你。不要问我为什么，我也很无奈"
+                    return reply.TextMsg(user, me, content).data
+                elif rec_msg.msg_type == "event":
+                    content = "哈喽，我现在还不能用哦~"
+                    return reply.TextMsg(user, me, content).data
+                else:
+                    print("位置类型", rec_msg.msg_type)
+                    return "success"
+            else:
+                print("暂不处理", rec_msg.msg_type)
+                return "success"
         else:
             print(request.method, request.form)
+            return ""
     except Exception as e:
         print(traceback.format_exc())
         return e
-
 
 
 if __name__ == "__main__":
